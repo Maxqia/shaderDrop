262124644f9656c13129caeb0064ee31b568057d
Max Qian <public@maxqia.com>
2018-10-13 20:21:31 -0700

    Readline and Restructuring Server

--- client.js
+++ client.js
@@ -2,32 +2,41 @@ var WebSocket = require('ws');
 var nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 var qrCode = require('qrcode-terminal');
+var readline = require('readline');
 
 var conn = new WebSocket("ws://127.0.0.1:8081");
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 var keyPair = nacl.sign.keyPair();
 var pubKeyString = nacl.util.encodeBase64(keyPair.publicKey);
 console.log(pubKeyString);
 qrCode.generate(pubKeyString);
 
-
 var currentLobby;
 var currentLobbyMembers;
 
 conn.onopen = function (event) {
     console.log("server connected!")
+    conn.send(JSON.stringify({
+        msgType: "clientType",
+        type: "client",
+        publicKey: nacl.util.encodeBase64(keyPair.publicKey),
+    }))
 }
 
 conn.onmessage = function (event) {
     var data = JSON.parse(event.data);
     switch (data.msgType) {
-        case "readData":
+        case "signData":
             console.log(data.bytesToSign);
             var bytesToSign = nacl.util.decodeBase64(data.bytesToSign);
             var signiture = nacl.sign(bytesToSign, keyPair.secretKey);
             conn.send(JSON.stringify({ 
-                        msgType : "sendPublicKey",
-                        publicKey : nacl.util.encodeBase64(keyPair.publicKey),
+                        msgType : "signReturn",
                         bytesSigned : nacl.util.encodeBase64(signiture),
             }));
             break;
@@ -53,3 +62,10 @@ conn.onmessage = function (event) {
             break;
     }
 }
+
+rl.on('line', (input) => {
+    conn.send(JSON.stringify({ 
+            msgType : "dataString",
+            string: input,
+    }));
+});
--- server.js
+++ server.js
@@ -14,32 +14,82 @@ var wsServer = new WebSocketServer( {
 
 var lobbies = new Object();
 
+
+function createRandomLobby() {
+    
+}
+
+function onScannerMessage(socket, data) {
+    
+}
+
+function onClientMessage(socket, data) {
+    switch(data.msgType) {
+        case "clientType" :
+            socket.publicKey = nacl.util.decodeBase64(data.publicKey);
+            socket.verified = false;
+            
+            socket.signBytes = nacl.randomBytes(nacl.sign.signatureLength);
+            console.log("sending random bytes to " + socket.ipPort);
+            socket.send(JSON.stringify({
+                msgType : "signData",
+                bytesToSign : nacl.util.encodeBase64(socket.signBytes),
+            }));
+            break;
+        case "signReturn":
+            var verifyBytes = nacl.sign.open(nacl.util.decodeBase64(data.bytesSigned), socket.publicKey);
+            if (!nacl.verify(socket.signBytes, verifyBytes)) {
+                throw "client failed verification, terminating session";
+            }
+            console.log("client " + socket.ipPort + " passed verification, public key : " + socket.publicKey);
+            socket.verified = true;
+            break;
+        case "dataString":
+            console.log(data.string);
+            //if (socket.verified) socket.lobby.sendToAll(data);
+    }
+}
+
+function onMessage(socket, incomingData) {
+    var data = JSON.parse(incomingData);
+    if (!data.hasOwnProperty("msgType")) throw "client " + socket.ipPort + " sent message without msgType!";
+    
+    switch(data.msgType) {
+        case "clientType":
+            socket.clientType = data.type;
+            break;
+    }
+    
+    if (!socket.hasOwnProperty("clientType")) return;
+    
+    if (socket.clientType == "client") {
+        onClientMessage(socket, data)
+    }
+
+    if (socket.clientType == "scanner") {
+        onScannerMessage(socket, data);
+    }
+
+}
+
 wsServer.on("connection", function(socket, request) {
-    var signBytes = nacl.randomBytes(nacl.sign.signatureLength);
-    var publicKey;
+    socket.ipPort = request.socket.remoteAddress + ":" + request.socket.remotePort;
+    console.log("client " + socket.ipPort + " connected");
     
-    console.log("client connected, sending random bytes : " + signBytes);
-    socket.send(JSON.stringify({
-        msgType : "readData",
-        bytesToSign : nacl.util.encodeBase64(signBytes),
-    }));
-    
-    socket.on("message", function (data) {
-        var data = JSON.parse(data);
-        
-        switch(data.msgType) {
-            case "sendPublicKey":
-                publicKey = nacl.util.decodeBase64(data.publicKey);
-                var verifyBytes = nacl.sign.open(nacl.util.decodeBase64(data.bytesSigned), publicKey);
-                if (!nacl.verify(signBytes, verifyBytes)) {
-                    console.log("client failed verification, terminating session");
-                    socket.terminate();
-                    return;
-                }
-                console.log("client passed verification, public key : " + data.publicKey);
-                break;
+    socket.on("message", function(data) {
+        try {
+            onMessage(socket, data);
+        } catch (error) {
+            console.log(error);
+            socket.terminate();
         }
     });
+    
+    socket.on("close", function(code, reason) {
+        console.log("client " + socket.ipPort + " closed due to : " + reason);
+    })
 });
+
+
 
 
