f4157e64a86cc9893e74429ac5bb09df2d9d485a
Max Qian <public@maxqia.com>
2019-10-04 02:51:15 -0700

    Rewrite Scanner in React

--- src/scanner.js
+++ src/scanner.js
@@ -1,44 +1,96 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ReactQrReader from 'react-qr-reader';
+
 const client = require("./client.js");
 const Instascan = require("@eventstag/instascan");
 
-var scanner = new Instascan.Scanner({ 
-    video: document.getElementById('preview'),
-    mirror: false,
-});
 
+function Client(props) {
+  return (
+    <button onClick={() => props.selectClient(props.id)}>
+      {props.id}
+    </button>
+  );
+}
 
+function File(props) {
+  return (
+    <button>
+    </button>
+  );
+}
 
-scanner.addListener('scan', function (content) {
-    console.log(content);
-    onScan(content);
+class ShaderDropScanner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clients: [],
+      files: [],
+      selectedObject: null,
+      isClient: true,
+    };
+    this.selectClient = this.selectClient.bind(this);
+  }
+  
+  
+  handleScan(content) {
+    if (content == null) return;
+    if (this.state.clients.includes(content)) return;
+    
+    console.log("new client: " + content);
+    this.addClient(content);
+    if (this.state.selectedObject != null && this.state.isClient) {
+      connectClients(this.state.selectedObject, content);
+    }
+  }
 
-});
-
-var clientList = [];
-function onScan(ID) {
+  addClient(ID) {
+    this.setState({
+      clients: this.state.clients.concat([ID]),
+    });
     client.sendMsg(ID, JSON.stringify({
         msgType : "scanned",
     }));
+  }
+  
+  selectClient(client) {
+    this.setState({selectedObject: client, isClient: true});
+  }
+  
+  render() {
+    const fileList = this.state.files.map((file) => {
+      return (<li> <File/> </li>);
+    });
+    const clientList = this.state.clients.map((id) => {
+      return (<li key={id}> <Client id={id} selectClient={this.selectClient}/> </li>);
+    });
     
-    clientList.push(ID);
-    if (clientList.length == 2) {
-      client.sendMsg(clientList[1], JSON.stringify({
-        msgType : "connect",
-        clientID : clientList[0],
-      }));
-     client.sendMsg(clientList[0], JSON.stringify({
-        msgType : "connect",
-        clientID : clientList[1],
-      }));
-    }
+    return (
+      <div>
+        <ReactQrReader
+          onError={(error) => console.error(error)}
+          onScan={(data) => this.handleScan(data)}
+          facingMode="environment"
+        />
+        <ul>{fileList}</ul>
+        <ul>{clientList}</ul>
+      </div>
+    );
+  }
 }
 
-Instascan.Camera.getCameras().then(function (cameras) {
-    if (cameras.length > 0) {
-      scanner.start(cameras[1]);
-    } else {
-      console.error('No cameras found.');
-    }
-}).catch(function (e) {
-    console.error(e);
-});
+ReactDOM.render(<ShaderDropScanner/>, document.getElementById('root'));
+
+function connectClients(cli1, cli2) {
+   client.sendMsg(cli1, JSON.stringify({
+      msgType : "connect",
+      clientID : cli2,
+   }));
+   client.sendMsg(cli2, JSON.stringify({
+      msgType : "connect",
+      clientID : cli1,
+   }));
+}
