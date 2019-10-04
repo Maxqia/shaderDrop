01b9f3fbe667cab45aa597c515ee9a510f782769
Max Qian <public@maxqia.com>
2019-10-04 04:11:37 -0700

    Refactor Server

--- src/server.js
+++ src/server.js
@@ -7,12 +7,7 @@ var WebSocketServer = WebSocket.Server;
 var nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-var clientList = require('./clientlist.js');
-
 /* Implements basic message passing (server-side) */
-
-
-//var HashMap = require('hashmap');
 
 /*const server = new https.createServer({
   cert: fs.readFileSync('cert.pem'),
@@ -25,6 +20,31 @@ var wsServer = new WebSocketServer( {
     port : 8081,
     clientTracking: true,
 } );
+
+var clientList = new Map();
+
+var number = 0;
+function getRandomID() {
+  number++;
+  return number.toString();
+}
+
+function socketSetup() {
+    this.randomBytes = nacl.randomBytes(nacl.sign.signatureLength);
+    this.verified = false;
+    this.publicKey = null;
+    this.stringID = getRandomID();
+    clientList.set(this.stringID, this);
+}
+
+// returns true if verification passed
+function socketVerify(publicKey, signedBytes) {
+    this.publicKey = publicKey;
+    var verifyBytes = nacl.sign.open(nacl.util.decodeBase64(signedBytes),  nacl.util.decodeBase64(publicKey));
+    this.verified = nacl.verify(this.randomBytes, verifyBytes);
+    delete this.randomBytes;
+    return this.verified;
+}
 
 wsServer.on("connection", function(socket, request) {
     socket.ipPort = request.socket.remoteAddress + ":" + request.socket.remotePort;
@@ -43,13 +63,16 @@ wsServer.on("connection", function(socket, request) {
         console.log("client " + socket.ipPort + " closed due to : " + reason);
     })
     
+
+    socket.setup = socketSetup;
+    socket.verify = socketVerify;
+    socket.setup();
     // send random bytes for client to sign
-    var client = clientList.newClient(socket);
     console.log("sending random bytes to " + socket.ipPort);
     socket.send(JSON.stringify({
         msgType : "signData",
-        bytesToSign : nacl.util.encodeBase64(client.randomBytes),
-        stringID : client.stringID,
+        bytesToSign : nacl.util.encodeBase64(socket.randomBytes),
+        stringID : socket.stringID,
     }));
 });
 
@@ -58,23 +81,21 @@ function onMessage(socket, incomingData) {
     var data = JSON.parse(incomingData);
     if (!data.hasOwnProperty("msgType")) throw "client " + socket.ipPort + " sent message without msgType!";
     
-    var client = clientList.getBySocket(socket);
     switch(data.msgType) {
         case "signReturn":
-            if (!client.verify(data.publicKey, data.bytesSigned)){
+            if (!socket.verify(data.publicKey, data.bytesSigned)){
                 throw "client failed verification, terminating session";
             }
-            console.log("client " + socket.ipPort + " passed verification, id : " + client.stringID + " : public key : " + client.publicKey);
+            console.log("client " + socket.ipPort + " passed verification, id : " + socket.stringID + " : public key : " + socket.publicKey);
             break;
         case "sendMsg":
-            var sendClient = clientList.getByID(data.stringID);
+            var sendClient = clientList.get(data.stringID);
             if (sendClient) {
-              sendClient.socket.send(JSON.stringify({
+              sendClient.send(JSON.stringify({
                 msgType: "msgRecv",
                 string: data.string,
-                fromID: client.stringID,
+                fromID: socket.stringID,
               }));
-              console.log(client);
             }
             break;
     }
