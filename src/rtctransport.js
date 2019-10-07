be35fb6cce35da7b86bc62e0a812a98b746f7164
Max Qian <public@maxqia.com>
2019-10-07 06:17:23 -0700

    Refactor WebRTC and WebSocket into Classes

---
+++ src/rtctransport.js
@@ -0,0 +1,110 @@
+'use strict';
+//import adapter from 'webrtc-adapter';
+import {RTCPeerConnection, RTCSessionDescription, RTCIceCandidate} from 'wrtc';
+
+export default class WebRTCTransport {
+  constructor() {
+    this.pc = null;
+    this.dc = null;
+
+    // public interface
+    this.msgRecv = (msg) => ();
+    this.sendMsg = this.sendMsg.bind(this);
+    this.sigRecv = this.recv.bind(this);
+    this.sigSend = null;
+    // end public interface
+    this.newPeerConnection();
+  }
+  
+  start() {
+    this.sendOffer();
+    return this.onOpenPromise;
+  }
+  
+  // send object to signaling peer
+  send(object) {
+    if (this.sigSend) {
+      this.sigSend(JSON.stringify(object));
+    } else {
+      throw "no signaling partner";
+    }
+  }
+  
+  recv(msg) {
+    var data = JSON.parse(incomingData);
+    if (!data.hasOwnProperty("msgType")) throw "recieved message without msgType!";
+    switch(data.msgType) {
+      case "offer":
+        this.sendAnswer(data.sdp).catch(reportError);
+        break;
+      case "new-ice-candidate":
+        var candidate = new RTCIceCandidate(data.candidate);
+        this.pc.addIceCandidate(candidate).catch(reportError);
+        break;
+      case "answer":
+        var desc = new RTCSessionDescription(data.sdp);
+        this.pc.setRemoteDescription(desc).catch(reportError);
+        break;
+    }
+  }
+  
+  newPeerConnection() {
+    this.pc = new RTCPeerConnection ({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org",
+        },
+      ],
+    });
+    this.pc.onicecandidate = this.handleIceCandidateEvent.bind(this);
+    this.pc.ondatachannel = this.recieveChannel.bind(this);
+    this.recieveChannel(this.pc.createDataChannel('stuff', { ordered: true }));
+  }
+  
+  recieveChannel(channel) {
+    this.dc = channel;
+    
+    this.onOpenPromise = new Promise((resolve, reject) => {
+      this.dc.onopen = (event) => {
+        this.log(event);
+        resolve();
+      };
+    });
+    this.dc.onclose = (event) => this.log(event);
+  }
+  
+  async sendOffer() {
+    var offer = await this.pc.createOffer();
+    await this.pc.setLocalDescription(offer);
+    this.send({
+      msgType:"offer",
+      sdp: this.pc.localDescription,
+    });
+  }
+
+  async sendAnswer(sdp) {
+    var desc = new RTCSessionDescription(sdp);
+    await this.pc.setRemoteDescription(desc);
+    var answer = await this.pc.createAnswer();
+    await this.pc.setLocalDescription(answer);
+    this.send({
+      msgType: "answer",
+      sdp: answer,
+    }); 
+  }
+
+  handleIceCandidateEvent(event) {
+    if (event.candidate) {
+      this.send({
+        msgType: "new-ice-candidate",
+        candidate: event.candidate,
+      });
+    }
+  }
+} 
+
+
+
+
+
+
---
+++ src/wstransport.js
@@ -0,0 +1,70 @@
+'use strict';
+const WebSocket = require('isomorphic-ws');
+const nacl = require('tweetnacl');
+nacl.util = require('tweetnacl-util');
+
+/* connects to our websocket infrasturcture for message passing */
+export default class WebSocketTransport {
+  constructor() {
+    this.keyPair = nacl.sign.keyPair();
+    this.publicKey = nacl.util.encodeBase64(keyPair.publicKey);
+  
+    // public interface
+    this.log = (msg) => console.log(msg);
+    this.msgRecv = (id, str) => this.log("recieved string from : " + id + " : " + str);
+    this.sendMsg = this.sendMsg.bind(this);
+    this.id = null;
+    this.onIDRecieved = () => {};
+    // end public interface
+  }
+  
+  connect() {
+    var url = typeof location !== 'undefined' ? "wss://"+location.host+"/websocket/" : "wss://127.0.0.1:8082/websocket/";
+    this.conn = new WebSocket(url);
+    this.conn.onmessage = onMessage.bind(this);
+    
+    return new Promise((resolve, reject) => {
+      this.conn.onopen = resolve;
+      this.conn.onerror = reject;
+    };
+  }
+  
+  getID() {
+    return new Promise((resolve, reject) => {
+      if (id) resolve(id);
+      this.onIDRecieved = () => resolve(id);
+    });
+  }
+  
+  sendMsg(id, str) {
+    conn.send(JSON.stringify({
+        msgType: "sendMsg",
+        stringID: id,
+        string: str,
+    }));
+  }
+  
+  // performs the handshake & passes the message to the message handler
+  onMessage(event) {
+    var data = JSON.parse(event.data);
+    switch (data.msgType) {
+      case "signData":
+        this.log(data.bytesToSign);
+        var bytesToSign = nacl.util.decodeBase64(data.bytesToSign);
+        var signiture = nacl.sign(bytesToSign, this.keyPair.secretKey);
+        this.conn.send(JSON.stringify({ 
+                    msgType : "signReturn",
+                    publicKey: nacl.util.encodeBase64(this.keyPair.publicKey),
+                    bytesSigned : nacl.util.encodeBase64(signiture),
+        }));
+        
+        this.log("id : " + data.stringID);
+        this.id = data.stringID;
+        this.gotID();
+        break;
+      case "msgRecv":
+        this.msgRecv(data.fromID, data.string);
+        break;
+    }
+  }
+}
