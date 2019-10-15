2f32a97b7e6d8ed3d97ff4f325af97d8b5b85709
Max Qian <public@maxqia.com>
2019-10-15 09:54:25 -0700

    transport: implement web browser support

---
+++ src/transport/webstr.js
@@ -0,0 +1,40 @@
+
+/* Web-Browser based Streams */
+
+export function createWritableStream(transport) {
+  let stream = new WritableStream({
+    start(controller) {
+    },
+    write(chunk, controller) {
+      // fragment chunk
+      const fragSize = 16384; // 16KiB (most common RTCDataChannel Message Maximum is 16KiB)
+      let currentPromise = transport.bufferLow();
+      
+      let bytePos = 0;
+      while (bytePos < chunk.length) {
+        let sendBytes = Math.min(fragSize, chunk.length - bytePos);
+        let fragChunk = chunk.subarray(bytePos, bytePos+sendBytes); // TODO what type are we reciving???
+        currentPromise = currentPromise.then(() => {
+          transport.send(fragChunk);
+          return transport.bufferLow();
+        });
+      }
+      return currentPromise;
+    },
+    close() {
+      transport.close();
+    },
+  }, new ByteLengthQueuingStragegy({ highWaterMark: 16384 }); // 16KiB queue
+  return stream;
+}
+
+export function createReadableStream(transport) {
+  let stream = new ReadableStream({
+    start(controller) {
+      transport.defaultHandler = (data) => {
+        controller.enqueue(data);
+      };
+    },
+  }, new ByteLengthQueuingStragegy({ highWaterMark: 16384 }); // 16KiB queue
+  return stream;
+}
